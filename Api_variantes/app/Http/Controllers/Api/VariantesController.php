b7ae6c30a4a2b11a7e387e89dbe21567e9548196
Blanca Lidia Castellanos Solis <53828168+BlancaCastellanos@users.noreply.github.com>
2022-05-10 16:12:14 -0700

    Add files via upload

---
+++ Api_variantes/app/Http/Controllers/Api/VariantesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Variante;
+
+class VariantesController extends Controller
+{
+    public function index(){
+        $variantes = Variante::select('id','lineage','common_countries','earliest_date', 
+        'designed_number', 'assigned_number', 'who_name')->
+            orderby('id', 'DESC')->
+            limit(20)->get();
+        return $variantes;
+    }
+
+    public function show($id) {
+        $variante = Variante::select('id', 
+            'description', 'evidence')->
+            where('id', $id)->first();
+        return $variante;
+    } 
+}
